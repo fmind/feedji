10ac8c4471fbc28384efc899e72c10a6fb07dbfb
fmind <fmind@users.noreply.github.com>
2017-04-07 14:27:55 +0200

    Added fetch entries

--- feedji.clj
+++ feedji.clj
@@ -1,60 +1,55 @@
 #!/usr/bin/env boot
 
-(set-env! :dependencies '[[org.clojars.scsibug/feedparser-clj "0.4.0"]
-                          [org.clojure/data.xml "0.0.8"]
-                          [me.raynes/conch "0.8.0"]
-                          [me.raynes/fs "1.4.6"]])
+(set-env! :dependencies '[[org.clojure/data.xml "0.0.8"]
+                          [com.rometools/rome "1.7.1"]
+                          [http-kit "2.2.0"]])
 
-(require '[feedparser-clj.core :refer [parse-feed]]
-         '[boot.cli :refer [defclifn]]
-         '[clojure.data.xml :as Xml]
-         '[clojure.java.shell :as sh]
-         '[clojure.string :as Str]
-         '[clojure.java.io :as io]
-         '[me.raynes.conch :as ch]
-         '[me.raynes.fs :as fs])
+(require '[boot.cli :refer [defclifn]]
+         '[org.httpkit.client :as H]
+         '[clojure.data.xml :as X]
+         '[clojure.java.io :as io])
+
+(import (com.rometools.rome.feed.synd SyndFeed)
+        (com.rometools.rome.io SyndFeedInput XmlReader))
 
 (defn feed? [element]
   (-> element :attrs :xmlUrl))
 
-(defn http->https [url]
-  (Str/replace url #"http://" "https://"))
+(defn print-err [& more]
+  (.println *err* (apply str more)))
 
-(defn fetch-entries [xmlUrl]
-  (println "TEST")
+(defn fetch-entries! [url]
   (try
-    (-> xmlUrl parse-feed :entries)
-   (catch Exception e ;; try https
-     (try
-       (-> xmlUrl http->https parse-feed :entries)
-     (catch Exception e
-       (println "error" xmlUrl)
-       nil))))) ;; give up
+    ;; only http-kit can follow redirects
+    (some->> @(H/get url {:as :stream}) :body io/reader
+              (.build (SyndFeedInput.)) .getEntries seq)
+  (catch Exception e
+    (print-err "ERROR: " e " >> " url))))
 
-(defn analyze-feed [{{:keys [title xmlUrl]} :attrs}]
-  (if-let [entries (fetch-entries xmlUrl)]
-    (let []
-      (println "ok" title)
-      )
-    [title nil]))
+(defn analyze-feed [feed]
+  (let [attrs (:attrs feed)
+        {:keys [title xmlUrl]} attrs
+        entries (fetch-entries! xmlUrl)]
+    (println title (count entries))
+    [title (count entries)]))
 
 (defn analyze-opml [opml]
-  (->> (Xml/parse-str opml)
-        (xml-seq)
-        (filter feed?)
-        rest
-        (map analyze-feed)))
+  (->> (xml-seq opml)
+       (filter feed?)
+       (map analyze-feed)))
 
-(defn format-analysis [analysis]
-  analysis)
+(defn format-output [output]
+  output)
 
 (defclifn
-  -main "A simple clojure script."
+  -main "OPML->statistics."
   [o out FILE file "output file."]
   (let [in (when-let [in (first *args*)] (io/as-file in))]
     (with-open [r (io/reader (or in *in*))]
       (with-open [w (io/writer (or out *out*))]
         (->> (slurp r)
+             X/parse-str
              analyze-opml
-             format-analysis
+             format-output
              (spit w))))))
+
