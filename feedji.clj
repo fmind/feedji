9d3ad4f8c963ff0cd9b631cc2d1f6c91c6609a53
fmind <fmind@users.noreply.github.com>
2017-04-11 17:23:22 +0200

    V0.5

--- feedji.clj
+++ feedji.clj
@@ -2,54 +2,64 @@
 
 (set-env! :dependencies '[[org.clojure/data.xml "0.0.8"]
                           [com.rometools/rome "1.7.1"]
+                          [clj-time "0.13.0"]
                           [http-kit "2.2.0"]])
 
 (require '[boot.cli :refer [defclifn]]
-         '[org.httpkit.client :as H]
-         '[clojure.data.xml :as X]
+         '[org.httpkit.client :as Http]
+         '[clojure.data.xml :as Xml]
+         '[clj-time.core :as Time]
+         '[clj-time.coerce :as TC]
          '[clojure.java.io :as io])
 
 (import (com.rometools.rome.feed.synd SyndFeed)
+        (com.rometools.rome.feed.synd SyndEntry)
         (com.rometools.rome.io SyndFeedInput XmlReader))
 
 (defn feed? [element]
-  (-> element :attrs :xmlUrl))
+  (get-in element [:attrs :xmlUrl]))
 
-(defn print-err [& more]
+(defn print-err! [& more]
   (.println *err* (apply str more)))
 
 (defn fetch-entries! [url]
   (try
-    ;; only http-kit can follow redirects
-    (some->> @(H/get url {:as :stream}) :body io/reader
+    ;; only http-kit can follow http redirections
+    (some->> @(Http/get url {:as :stream}) :body io/reader
               (.build (SyndFeedInput.)) .getEntries seq)
   (catch Exception e
-    (print-err "ERROR: " e " >> " url))))
+    (print-err! "ERROR: " e " >> " url))))
+
+(def parse-opml Xml/parse-str)
+
+(defn entries->stats [entries]
+  (if (or (nil? entries) (zero? (count entries))) nil
+      (let [dates (map #(TC/from-date (.getPublishedDate %)) entries)
+            from (last dates), to (first dates)
+            interval (Time/interval from to)
+            hours (Time/in-hours interval)
+            hours (if (zero? hours) 1 hours)
+            total (count entries)]
+        {:entries total
+         :in-hours hours
+         :per-hour (float (/ total hours))})))
 
 (defn analyze-feed [feed]
   (let [attrs (:attrs feed)
         {:keys [title xmlUrl]} attrs
-        entries (fetch-entries! xmlUrl)]
-    (println title (count entries))
-    [title (count entries)]))
+        entries (fetch-entries! xmlUrl)
+        stats (entries->stats entries)]
+    [title stats]))
 
-(defn analyze-opml [opml]
-  (->> (xml-seq opml)
-       (filter feed?)
-       (map analyze-feed)))
+(defn analyze-opml [xml]
+  (->> (xml-seq xml)
+       (filterv feed?)
+       (pmap analyze-feed)))
 
-(defn format-output [output]
-  output)
+(defn format-output [analysis]
+  (->> analysis (into {}) pr-str))
 
-(defclifn
-  -main "OPML->statistics."
-  [o out FILE file "output file."]
-  (let [in (when-let [in (first *args*)] (io/as-file in))]
-    (with-open [r (io/reader (or in *in*))]
-      (with-open [w (io/writer (or out *out*))]
-        (->> (slurp r)
-             X/parse-str
-             analyze-opml
-             format-output
-             (spit w))))))
+(def process-opml (comp analyze-opml parse-opml))
 
+(defclifn -main "Compute statistics from an OPML file." []
+  (->> (slurp *in*) process-opml format-output (spit *out*)))
